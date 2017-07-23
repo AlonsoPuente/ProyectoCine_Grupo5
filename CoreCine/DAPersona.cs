5c6eeaecdd5bd2889cdd9686c343da2e4c0dcd09
Alonso Puente <Alonso Puente@HP>
2017-07-23 12:00:30 -0500

    Agregando Controladora y Data Access de Persona.

---
+++ CoreCine/DAPersona.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCine
+{
+    public class DAPersona
+    {
+        /// Listado de Persona
+        //////////////////////////////////////////////////////
+        static public List<Persona> ListadoPersona()
+        {
+            List<Persona> listadoPersona = new List<Persona>();
+            using (var data = new CineDATABASEEntities1())
+            {
+
+                listadoPersona = data.Persona.ToList();
+
+            }
+
+            return listadoPersona;
+        }
+
+        /// Actualizador de Persona
+        //////////////////////////////////////////////////////
+        static public bool ActualizarPersona(Persona persona) // referido a objeto
+        {
+            bool exito = true;
+            try
+            {
+                using (var data = new CineDATABASEEntities1())
+                {
+                    // realizar la consulta y actualizar
+                    Persona actual = data.Persona.Where(x => x.CodPersona == persona.CodPersona).FirstOrDefault();// alias
+                    actual.Paterno = persona.Paterno;
+                    actual.Materno = persona.Materno;
+                    actual.Nombres = persona.Nombres;
+                    actual.Direccion = persona.Direccion;
+                    actual.Email = persona.Email;
+                    actual.FechaNacimiento = persona.FechaNacimiento;
+                    actual.NroDocumento = persona.NroDocumento;
+                    actual.foto = persona.foto;
+                    actual.doc = persona.doc;
+                    data.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                exito = false;
+            }
+            return exito;
+
+        }
+
+        /// Crear Persona
+        //////////////////////////////////////////////////////
+
+        /// Eliminar Persona
+        //////////////////////////////////////////////////////
+    }
+}
---
+++ MVCAjax_Listado/Areas/Persona/Controllers/MainController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CoreCine;
+
+namespace MVCAjax_Listado.Areas.Persona.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Persona/Main
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListadoPersona()
+        {
+            return PartialView(DAPersona.ListadoPersona());
+        }
+
+        ///////////////////////// Actualizar Cine        
+        public ActionResult ActualizarPersona(int CodPersona)
+        {
+            CoreCine.Persona persona = DAPersona.ListadoPersona().Where(x => x.CodPersona == CodPersona).FirstOrDefault();
+            return View(persona);
+        }
+
+        [HttpPost]
+        public ActionResult ActualizarPersona(CoreCine.Persona persona)
+        {
+            //para cargar la data
+            bool exito = DAPersona.ActualizarPersona(persona);
+            return RedirectToAction("Index");
+
+        }
+    }
+}
