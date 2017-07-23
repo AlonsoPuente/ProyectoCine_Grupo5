e4c24fc809f5176d591bbaad823213da784ae3b0
Kardas <user@OPTIMUS>
2017-07-22 21:34:46 -0500

    Actualizado Andres

---
+++ CoreCine/DAInterprete.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCine
+{
+    public class DAInterprete
+    {
+        static public List<Interprete> listadoInterprete()
+        {
+            List<Interprete> listado = new List<Interprete>();
+
+            using (var data = new CineDATABASEEntities1())
+
+            {
+                listado = data.Interprete.ToList();
+            }
+
+            return listado;
+        }
+        static public bool RegistrarInterprete(Interprete interprete)
+        {
+            bool exito = true;
+
+            try
+            {
+                using (var data = new CineDATABASEEntities1())
+                {
+                    data.Interprete.Add(interprete);
+                    data.SaveChanges();
+                }
+            }
+            catch
+            {
+                exito = false;
+            }
+
+            return exito;
+        }
+        static public bool EliminarInterprete(int Codinterprete ) // referido a borrar
+        {
+            bool exito = true;
+            try
+            {
+
+                using (var data = new CineDATABASEEntities1())
+                {
+                    Interprete interprete = data.Interprete.Where(x => x.CodInterprete == Codinterprete).FirstOrDefault();// alias
+
+                    data.Interprete.Remove(interprete);
+                    data.SaveChanges();
+
+                }
+            }
+            catch (Exception)
+            {
+
+                exito = false;
+            }
+
+            return exito;
+        }
+        static public bool ActualizarInterprete(Interprete interprete)
+        {
+            bool exito = true; 
+            try
+            {
+                using (var data = new CineDATABASEEntities1())
+                {
+
+                    Interprete actual = data.Interprete.Where(x => x.CodInterprete == interprete.CodInterprete).FirstOrDefault();
+
+                    actual.CodInterprete = interprete.CodInterprete;
+                    actual.Paterno = interprete.Paterno;
+                    actual.Materno = interprete.Materno;
+                    actual.Nombres = interprete.Nombres;
+                    actual.Nacionalidad = interprete.Nacionalidad;
+                    actual.Foto = interprete.Foto;
+                    actual.Bio = interprete.Bio;
+
+
+                    data.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                exito = false;
+            }
+            return exito;
+
+        }
+    }
+}
--- MVCAjax_Listado/Areas/Interprete/Controllers/MainController.cs
+++ MVCAjax_Listado/Areas/Interprete/Controllers/MainController.cs
@@ -17,14 +17,37 @@ namespace MVCAjax_Listado.Areas.Interprete.Controllers
 
         public ActionResult Listado()
         {
-            ViewBag.ListadoProtagonista = DAGeneral.ListadoProtagonista();
-            return PartialView(DAGeneral.ListadoInterprete());
+            ViewBag.ListadoInterprete = DAInterprete.listadoInterprete();
+            return PartialView(DAInterprete.listadoInterprete());
         }
+        public ActionResult RegistrarInterprete()
+        {
+            return PartialView();
+        }
+        public ActionResult EliminarInterprete(int ID)
+        {
+            bool exito = DAInterprete.EliminarInterprete(ID);
+            return RedirectToAction("Index");
+        }
+        //public ActionResult EditarInterprete(int ID)
+        //{
+        //    Interprete proyecto = DAInterprete.listadoInterprete().Where(x => x.CodInterprete == ID).FirstOrDefault();
+        //    return View(proyecto);
+        //}
 
-        
+        //public ActionResult EditarProyecto(Interprete interprete)
+        //{
+        //    //para cargar la data
+        //    bool exito = DAInterprete.ActualizarInterprete(interprete);
+        //    return RedirectToAction("Index");
 
-       
+        //}
+
+
+
+
+
     }
 
-    
+
 }
