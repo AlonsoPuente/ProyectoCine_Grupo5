9a0778d9a49ba3ad1e500de51965cca3bb42befe
Anthony <user@OPTIMUS>
2017-07-23 23:09:01 -0500

    Completo

---
+++ MVCAjax_Listado/Areas/Principal/Controllers/MainController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAjax_Listado.Areas.Principal.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Principal/Main
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
