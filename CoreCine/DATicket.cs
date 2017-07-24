15afb617372394feae6a3860ca4342f383343d61
Anthony <user@OPTIMUS>
2017-07-23 22:15:31 -0500

    Ultima tabla

---
+++ CoreCine/DATicket.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCine
+{
+    public class DATicket
+    {
+
+        static public List<Ticket> ListadoTicket()
+        {
+            List<Ticket> listadoTicket = new List<Ticket>();
+            using (var data = new CineDATABASEEntities())
+            {
+
+                listadoTicket = data.Ticket.ToList();
+
+            }
+            return listadoTicket;
+        }
+
+        /// Registrar Ticket
+        //////////////////////////////////////////////////////
+
+        static public bool RegistrarTicket(Ticket Ticket)
+        {
+            bool exito = true;
+
+            try
+            {
+                using (var data = new CineDATABASEEntities())
+                {
+                    data.Ticket.Add(Ticket);
+                    data.SaveChanges();
+                }
+            }
+            catch
+            {
+                exito = false;
+            }
+
+            return exito;
+        }
+
+        /// Actualizar Ticket
+        //////////////////////////////////////////////////////
+        static public bool ActualizarTicket(Ticket Ticket) // referido a objeto
+        {
+            bool exito = true;
+            try
+            {
+                using (var data = new CineDATABASEEntities())
+                {
+                    // realizar la consulta y actualizar
+                    Ticket actual = data.Ticket.Where(x => x.CodTicket == Ticket.CodTicket).FirstOrDefault();// alias
+                    actual.Descripcion = Ticket.Descripcion;
+                    actual.CodAsiento = Ticket.CodAsiento;
+                    actual.CodPersona = Ticket.CodPersona;
+                    actual.CodFuncion = Ticket.CodFuncion;
+                    data.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                exito = false;
+            }
+            return exito;
+
+        }
+
+        /// Eliminar Pelicula
+        //////////////////////////////////////////////////////
+        static public bool EliminarTicket(int CodTicket) // referido a borrar
+        {
+            bool exito = true;
+            try
+            {
+
+                using (var data = new CineDATABASEEntities())
+                {
+                    Ticket Ticket = data.Ticket.Where(x => x.CodTicket == CodTicket).FirstOrDefault();// alias
+
+                    data.Ticket.Remove(Ticket);
+                    data.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+                exito = false;
+            }
+
+            return exito;
+        }
+
+    }
+}
---
+++ MVCAjax_Listado/Areas/Ticket/Controllers/MainController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CoreCine;
+using System.Web.Mvc;
+
+namespace MVCAjax_Listado.Areas.Ticket.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Ticket/Main
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListadoTicket()
+        {
+            ViewBag.ListadoAsiento = DAAsiento.ListadoAsiento();
+            ViewBag.ListadoPersona = DAPersona.ListadoPersona();
+            ViewBag.ListadoFuncion = DAFuncion.ListadoFuncion();
+            return PartialView(DATicket.ListadoTicket());
+        }
+
+        ///////////////////////// Actualizar Ticket        
+        public ActionResult ActualizarTicket(int CodTicket)
+        {
+            CoreCine.Ticket Ticket = DATicket.ListadoTicket().Where(x => x.CodTicket == CodTicket).FirstOrDefault();
+            return View(Ticket);
+        }
+
+        [HttpPost]
+        public ActionResult ActualizarTicket(CoreCine.Ticket ti)
+        {
+            //para cargar la data
+            bool exito = DATicket.ActualizarTicket(ti);
+            return RedirectToAction("Index");
+
+        }
+
+        ////////////////////////// Eliminar Ticket
+
+        public ActionResult Eliminar(int CodTicket)
+        {
+            bool exito = DATicket.EliminarTicket(CodTicket);
+
+            return RedirectToAction("Index");
+
+
+        }
+
+        ////////////////////////// Registrar Ticket
+
+
+        public JsonResult GrabarTicket(string Descripcion, int CodAsiento, int CodPersona,
+            int CodFuncion)
+        {
+            CoreCine.Ticket ticket = new CoreCine.Ticket();
+
+
+            ticket.Descripcion = Descripcion;
+            ticket.CodAsiento = CodAsiento;
+            ticket.CodPersona = CodPersona;
+            ticket.CodFuncion = CodFuncion;
+
+            // texto satisfactorio
+
+            bool exito = DATicket.RegistrarTicket(ticket);
+
+            string mensaje = string.Empty;
+
+            if (exito)
+            {
+                mensaje = "Registro satisfactorio";
+            }
+            else
+            {
+                mensaje = "Ha ocurrido un error. Intente nuevamente";
+            }
+
+            return Json(mensaje, JsonRequestBehavior.AllowGet);
+
+        }
+
+        public ActionResult FormTicket()
+        {
+
+            return PartialView();
+
+        }
+
+
+
+    }
+}
